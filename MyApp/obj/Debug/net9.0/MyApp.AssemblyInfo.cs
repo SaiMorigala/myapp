95ee88813fdeb8525750313b5bc5c4ba43c1c26a
smorigala <smorigala@posigen.com>
2025-12-20 00:18:27 +0530

    Add Dockerfile for .NET API

--- MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a005ad52aecc51e3e194cdc9b0bf75a16ea42d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dde0e0eafe547d4004aeb7498671c23c60d4fc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
